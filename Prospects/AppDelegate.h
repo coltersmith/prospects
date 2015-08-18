50ada8de3da986125508847ceaaf535d04e75359
Robert DeLuca <robertdeluca@Roberts-MBP.frontier.com>
2015-08-18 15:14:11 -0400

    Big elite man

--- Prospects/AppDelegate.h
+++ Prospects/AppDelegate.h
@@ -5,6 +5,8 @@
 //  Created by Colter Smith on 8/11/15.
 //  Copyright (c) 2015 Colter Smith. All rights reserved.
 //
+// Hello my name is Robert and I'm a big elite boss. 
+//
 
 #import <UIKit/UIKit.h>
 
